2d4e05853d3b01381acc6164572d888460801a83
HimanshiDMaithani <himanshi.d.maithani@gmail.com>
2023-06-12 13:24:13 +0100

    first commit

---
+++ InsuranceAPI/Markel.Data/Interface/IClaimRepository.cs
@@ -0,0 +1,16 @@
+﻿using Markel.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markel.Data.Interface
+{
+     public interface IClaimRepository
+    {
+        Claim GetClaim(string assuredName);
+        List<Claim> GetClaimsByCompanyId(int companyId);
+        void UpdateClaim(Claim claim);
+    }
+}
---
+++ InsuranceAPI/Markel.Data/Interface/ICompanyRepository.cs
@@ -0,0 +1,15 @@
+﻿using Markel.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markel.Data.Interface
+{
+    public interface ICompanyRepository
+    {
+        Company GetCompany(int companyId);
+        bool HasActiveInsurancePolicy(int companyId);
+    }
+}
